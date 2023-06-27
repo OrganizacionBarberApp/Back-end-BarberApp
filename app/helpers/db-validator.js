21ee9af33c156677a33b610f360104e47b348e50
Jefferson Izquierdo <jeffersondavid803@gmail.com>
2023-06-27 15:47:53 -0500

    Consulta de usuario dinamica

--- app/helpers/db-validator.js
+++ app/helpers/db-validator.js
@@ -3,8 +3,8 @@ const serviceUser = require("../controllers/user.controller");
 
 const emailExists = async(email = '') => {
     // Verificar si el correo existe
-   
-    const existsEmail = await serviceUser.consultUserEmail(email);
+
+    const existsEmail = await serviceUser.consultUserByEmailOrId(email);
     console.log(existsEmail)
     if(existsEmail){
         throw new Error(`El correo ${email} ya está registrado`);
